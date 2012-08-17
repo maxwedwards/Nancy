8fb6fc97c4ffe83f003ac9d2ac4520295eda18e7
Max Edwards <youwontforgetthis@gmail.com>
2012-08-17 13:49:57 +0100

    Updated Unit tests

--- src/Nancy.Tests/Unit/ModelBinding/DefaultBinderFixture.cs
+++ src/Nancy.Tests/Unit/ModelBinding/DefaultBinderFixture.cs
@@ -468,6 +468,28 @@ namespace Nancy.Tests.Unit.ModelBinding
             result.IntProperty.ShouldEqual(0);
         }
 
+        [Fact]
+        public void Should_be_able_to_record_which_properties_have_been_bound()
+        {
+            // Given
+            var binder = this.GetBinder();
+
+            var context = CreateContextWithHeader("Content-Type", new[] { "application/xml" });
+            context.Request.Query["StringProperty"] = "Test";
+            context.Parameters["IntProperty"] = "0";
+
+            // When
+            var result = (TestModel)binder.Bind(context, typeof(TestModel));
+
+            // Then
+            binder.BoundProperties.ShouldEqual(
+                (from prop in result.GetType().GetProperties()
+                 where prop.Name == "StringProperty" || prop.Name == "IntProperty"
+                 select prop).ToList()
+                );
+
+        }
+
         private IBinder GetBinder(IEnumerable<ITypeConverter> typeConverters = null, IEnumerable<IBodyDeserializer> bodyDeserializers = null, IFieldNameConverter nameConverter = null, BindingDefaults bindingDefaults = null)
         {
             var converters = typeConverters ?? new ITypeConverter[] { };
